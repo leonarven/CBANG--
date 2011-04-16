6473556a403b2ab16fdfdb5e68e0fea07da0ecf7
Sante H <hyrsky@riseup.net>
2011-04-16 04:57:49 +0300

    serveri vastaa kaikill

--- player.hpp
+++ player.hpp
@@ -4,20 +4,26 @@
 #include "common.hpp"
 #include "card.hpp"
 
+#include <SFML/Network.hpp>
+
 class player
 {
     public:
-    player(int _role, int _character, int _mHealth) : role(_role), character(_character), mHealth(_mHealth), health(_mHealth)
+    player(sf::SocketTCP _socket, int id, int _role, int _character, int _mHealth) :
+        role(_role), character(_character), mHealth(_mHealth), health(_mHealth), socket(_socket)
     {
         for (int i = 0; i < _mHealth; i++)
         {
-
+            //TODO: arvo kortit tässä
         }
     }
-    int		getHealt()		{ return this->health; }
-    int		getMHealt()		{ return this->mHealth; }
-    int		getRole()		{ return this->role; }
-    int		getCharacter()	{ return this->character; }
+
+    sf::SocketTCP getSocket()   { return this->socket; }
+    int     getId()             { return this->id; }
+    int		getHealt()		    { return this->health; }
+    int		getMHealt()		    { return this->mHealth; }
+    int		getRole()		    { return this->role; }
+    int		getCharacter()	    { return this->character; }
     void	getCard(CARDS _card, bool table = false) {
         if (table) {
             this->table.push_back(card(_card));
@@ -29,11 +35,17 @@ class player
     }
     void setHealth(int nHealth) { this->health = nHealth; }
 
-    private:
+
+
+private:
+
+    sf::SocketTCP socket;
+    int id;
+
     std::vector<card>	table;
     std::vector<card>	hand;
     std::vector<card*>	cards;
-    const int role, character, mHealth;
+    int role, character, mHealth;
     int health;
 };
 
